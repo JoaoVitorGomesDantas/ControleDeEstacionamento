ad00ecf0c2e13529f56fc61b56d27f63762d8019
JoaoVitorGomesDantas <92762467+JoaoVitorGomesDantas@users.noreply.github.com>
2021-10-23 18:34:55 -0300

    Add files via upload

---
+++ Estacionamento.c
@@ -0,0 +1,79 @@
+
+///////////////////////////////////////////////////////////////////////////////
+///               Universidade Federal do Rio Grande do Norte               ///
+///                   Centro de Ensino Superior do Seridó                   ///
+///                 Departamento de Computação e Tecnologia                 ///
+///                    Disciplina DCT1106 -- Programação                    ///
+///              Projeto Sistema de Controle de Estacionamento              ///
+///                  Developed by @joaovitorgd - Out, 2021                  ///
+///////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+
+/////
+// Assinatura das funções
+void telaInfo(void);
+void telaCriacao(void);
+
+/////
+// Programa principal
+int main(void) {
+    telaInfo();
+    telaCriacao();
+    return 0;
+}
+
+/////
+// Funções
+
+void telaInfo(void) {
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///               Universidade Federal do Rio Grande do Norte               ///\n");
+    printf("///                   Centro de Ensino Superior do Seridó                   ///\n");
+    printf("///                 Departamento de Computação e Tecnologia                 ///\n");
+    printf("///                    Disciplina DCT1106 -- Programação                    ///\n");
+    printf("///              Projeto Sistema de Controle de Estacionamento              ///\n");
+    printf("///                  Developed by @joaovitorgd - Out, 2021                  ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///        = = = = = Sistema de Controle de Estacionamento = = = = =        ///\n");
+    printf("///                                                                         ///\n");
+    printf("/// Projeto da disciplina DCT1106 - Programação, para criação de um sistema ///\n");
+    printf("/// de controle de estacionamentos ao qual tem por objetivos desenvolver um ///\n");
+    printf("/// programa de computador que possibilite aos administradores de uma empre-///\n");
+    printf("/// sa controlar a entrada e saída de veículos, além de registrar o fluxo   ///\n");
+    printf("/// financeiro dessa empresa, facilitando a gestão do negócio.              ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
+
+void telaCriacao(void) {
+    printf("\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///               Universidade Federal do Rio Grande do Norte               ///\n");
+    printf("///                   Centro de Ensino Superior do Seridó                   ///\n");
+    printf("///                 Departamento de Computação e Tecnologia                 ///\n");
+    printf("///                    Disciplina DCT1106 -- Programação                    ///\n");
+    printf("///              Projeto Sistema de Controle de Estacionamento              ///\n");
+    printf("///                  Developed by @joaovitorgd - Out, 2021                  ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                         ///\n");
+    printf("///        = = = = = Sistema de Controle de Estacionamento = = = = =        ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            Este projeto exemplo foi desenvolvido por:                   ///\n");
+    printf("///                                                                         ///\n");
+    printf("///            Aluno: João Vitor Gomes Dantas                               ///\n");
+    printf("///            E-mail: joao.vitor.dantas.085@ufrn.edu.br                    ///\n");
+    printf("///            Redes sociais: @joaovitorgd                                  ///\n");
+    printf("///            Repositório: https://github.com/JoaoVitorGomesDantas         ///\n");
+    printf("///                         /ControleDeEstacionamento                       ///\n");
+    printf("///                                                                         ///\n");
+    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+}
