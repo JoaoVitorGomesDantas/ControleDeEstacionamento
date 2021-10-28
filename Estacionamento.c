1d56c05aa2341e3601be9c0563d4b2628bd64883
JoaoVitorGomesDantas <92762467+JoaoVitorGomesDantas@users.noreply.github.com>
2021-10-28 18:24:05 -0300

    Update Estacionamento.c

--- Estacionamento.c
+++ Estacionamento.c
@@ -33,26 +33,26 @@ int main(void) {
 
 void telaInfo(void) {
     printf("\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                         ///\n");
-    printf("///               Universidade Federal do Rio Grande do Norte               ///\n");
-    printf("///                   Centro de Ensino Superior do Seridó                   ///\n");
-    printf("///                 Departamento de Computação e Tecnologia                 ///\n");
-    printf("///                    Disciplina DCT1106 -- Programação                    ///\n");
-    printf("///              Projeto Sistema de Controle de Estacionamento              ///\n");
-    printf("///                  Developed by @joaovitorgd - Out, 2021                  ///\n");
-    printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
-    printf("///                                                                         ///\n");
-    printf("///        = = = = = Sistema de Controle de Estacionamento = = = = =        ///\n");
-    printf("///                                                                         ///\n");
-    printf("/// Projeto da disciplina DCT1106 - Programação, para criação de um sistema ///\n");
-    printf("/// de controle de estacionamentos ao qual tem por objetivos desenvolver um ///\n");
-    printf("/// programa de computador que possibilite aos administradores de uma empre-///\n");
-    printf("/// sa controlar a entrada e saída de veículos, além de registrar o fluxo   ///\n");
-    printf("/// financeiro dessa empresa, facilitando a gestão do negócio.              ///\n");
-    printf("///                                                                         ///\n");
-    printf("///////////////////////////////////////////////////////////////////////////////\n");
+    printf("/////////////////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                                   ///\n");
+    printf("///                    Universidade Federal do Rio Grande do Norte                    ///\n");
+    printf("///                        Centro de Ensino Superior do Seridó                        ///\n");
+    printf("///                      Departamento de Computação e Tecnologia                      ///\n");
+    printf("///                         Disciplina DCT1106 -- Programação                         ///\n");
+    printf("///                   Projeto Sistema de Controle de Estacionamento                   ///\n");
+    printf("///                       Developed by @joaovitorgd - Out, 2021                       ///\n");
+    printf("///                                                                                   ///\n");
+    printf("/////////////////////////////////////////////////////////////////////////////////////////\n");
+    printf("///                                                                                   ///\n");
+    printf("///             ========= Sistema de Controle de Estacionamento =========             ///\n");
+    printf("///                                                                                   ///\n");
+    printf("///      Projeto da disciplina DCT1106 - Programação, para criação de um sistema      ///\n");
+    printf("///      de controle de estacionamentos ao qual tem por objetivos desenvolver um      ///\n");
+    printf("///      programa de computador que possibilite aos administradores de uma empre-     ///\n");
+    printf("///      sa controlar a entrada e saída de veículos, além de registrar o fluxo        ///\n");
+    printf("///      financeiro dessa empresa, facilitando a gestão do negócio.                   ///\n");
+    printf("///                                                                                   ///\n");
+    printf("/////////////////////////////////////////////////////////////////////////////////////////\n");
     printf("\n");
 }
 
