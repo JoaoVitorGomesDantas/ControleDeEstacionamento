4384fa338b8ca2dfa36b88883e5bdd2087363f3c
JoaoVitorGomesDantas <92762467+JoaoVitorGomesDantas@users.noreply.github.com>
2021-11-29 16:54:32 -0300

    Create Funcionarios.h

---
+++ Funcionarios.h
@@ -0,0 +1,17 @@
+///////////////////////////////////////////////////////////////////////////////
+///               Universidade Federal do Rio Grande do Norte               ///
+///                   Centro de Ensino Superior do Seridó                   ///
+///                 Departamento de Computação e Tecnologia                 ///
+///                    Disciplina DCT1106 -- Programação                    ///
+///              Projeto Sistema de Controle de Estacionamento              ///
+///   Inspirado em: @flgorgonio https://github.com/flgorgonio/linguasolta   ///
+///                  Developed by @joaovitorgd - Out, 2021                  ///
+///////////////////////////////////////////////////////////////////////////////
+
+
+void moduloFuncionarios(void);
+char telaFuncionarios(void);
+void telaCadastrarFunc(void);
+void telaPesquisarFunc(void);
+void telaAtualizarFunc(void);
+void telaExcluirFunc(void);
