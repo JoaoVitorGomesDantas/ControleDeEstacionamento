b8b30d8c5e0435c602b624ad9d024e82bf474f56
JoaoVitorGomesDantas <92762467+JoaoVitorGomesDantas@users.noreply.github.com>
2021-11-29 16:52:04 -0300

    Create Cliente.h

---
+++ Cliente.h
@@ -0,0 +1,17 @@
+///////////////////////////////////////////////////////////////////////////////
+///               Universidade Federal do Rio Grande do Norte               ///
+///                   Centro de Ensino Superior do Seridó                   ///
+///                 Departamento de Computação e Tecnologia                 ///
+///                    Disciplina DCT1106 -- Programação                    ///
+///              Projeto Sistema de Controle de Estacionamento              ///
+///   Inspirado em: @flgorgonio https://github.com/flgorgonio/linguasolta   ///
+///                  Developed by @joaovitorgd - Out, 2021                  ///
+///////////////////////////////////////////////////////////////////////////////
+
+
+void moduloCliente(void);
+char telaCliente(void);
+void telaCadastrar(void);
+void telaPesquisar(void);
+void telaAtualizar(void);
+void telaExcluir(void);
