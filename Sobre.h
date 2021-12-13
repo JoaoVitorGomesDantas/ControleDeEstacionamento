83b6b32eb0647e99c9771a24e6fdb4afa7a7f839
JoaoVitorGomesDantas <92762467+JoaoVitorGomesDantas@users.noreply.github.com>
2021-12-13 20:35:00 -0300

    Create Sobre.h

---
+++ Sobre.h
@@ -0,0 +1,14 @@
+
+///////////////////////////////////////////////////////////////////////////////
+///               Universidade Federal do Rio Grande do Norte               ///
+///                   Centro de Ensino Superior do Seridó                   ///
+///                 Departamento de Computação e Tecnologia                 ///
+///                    Disciplina DCT1106 -- Programação                    ///
+///              Projeto Sistema de Controle de Estacionamento              ///
+///   Inspirado em: @flgorgonio https://github.com/flgorgonio/linguasolta   ///
+///                  Developed by @joaovitorgd - Out, 2021                  ///
+///////////////////////////////////////////////////////////////////////////////
+
+
+void telaInfo(void);
+void telaCriacao(void);
