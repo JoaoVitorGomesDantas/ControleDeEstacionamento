52dc114f078df873902d13bfc9249835c946108e
JoaoVitorGomesDantas <92762467+JoaoVitorGomesDantas@users.noreply.github.com>
2021-12-13 20:31:15 -0300

    Create Aluguel.h

---
+++ Aluguel.h
@@ -0,0 +1,15 @@
+
+///////////////////////////////////////////////////////////////////////////////
+///               Universidade Federal do Rio Grande do Norte               ///
+///                   Centro de Ensino Superior do Seridó                   ///
+///                 Departamento de Computação e Tecnologia                 ///
+///                    Disciplina DCT1106 -- Programação                    ///
+///              Projeto Sistema de Controle de Estacionamento              ///
+///   Inspirado em: @flgorgonio https://github.com/flgorgonio/linguasolta   ///
+///                  Developed by @joaovitorgd - Out, 2021                  ///
+///////////////////////////////////////////////////////////////////////////////
+
+void moduloAluguel(void);
+char telaAluguel(void);
+void telaVagas(void);
+void telaAlugando(void);
