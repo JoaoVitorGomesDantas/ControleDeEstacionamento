c8abf6486aa68f08da989b0d3538fa4708713f07
JoaoVitorGomesDantas <92762467+JoaoVitorGomesDantas@users.noreply.github.com>
2022-01-20 20:45:28 -0300

    Update Aluguel.c

--- Aluguel.c
+++ Aluguel.c
@@ -14,6 +14,8 @@
 #include <unistd.h>
 #include "Aluguel.h"
 
+typedef struct aluguel Aluguel;
+
 ///////////////////////////////////////
 ////// Funções do Módulo Aluguel //////
 ///////////////////////////////////////
@@ -47,10 +49,10 @@ char telaAluguel(void) {
     printf("///               2 -  Aluguel                                                        ///\n");
     printf("///               0 -  Voltar para meunu principal                                    ///\n");
     printf("///                                                                                   ///\n");
-    printf("///            Escolha a opção que você deseja:                                       ///\n");
-    scanf("%c", &opc);
-    printf("///                                                                                   ///\n");
     printf("/////////////////////////////////////////////////////////////////////////////////////////\n");
+    printf("\n");
+    printf("               Escolha a opção que você deseja:                                          \n");
+    scanf("%c", &opc);
     printf("\n");
     printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
     getchar();
@@ -82,15 +84,8 @@ void telaVagas(void) {
 }
 
 void telaAlugando(void) {
-    char nome[61];
-    char cpf[14];
-    char dte[11];
-    char dts[11];
-    char tpvei[40];
-    char mdvei[40];
-    char placa[12];
-    char valor[12];
-
+    Aluguel* alg;
+    alg = (Aluguel*) malloc(sizeof(Aluguel));
 
     printf("\n");
     printf("/////////////////////////////////////////////////////////////////////////////////////////\n");
@@ -103,42 +98,41 @@ void telaAlugando(void) {
     printf("///                                                                                   ///\n");
     printf("///                                                                                   ///\n");
     printf("///          Nome do cliente:                                                         ///\n");
-    scanf("%[A-ZÁÉÍÓÚÂÊÔÇÀÃÕ a-záéíóúâêôçàãõ]", nome);
+    scanf("%[A-ZÁÉÍÓÚÂÊÔÇÀÃÕ a-záéíóúâêôçàãõ]", alg->nome);
     getchar();
 
     printf("///          CPF:                                                                     ///\n");
-    scanf("%[0-9]", cpf);
+    scanf("%[0-9]", alg->cpf);
     getchar();
 
     printf("///          Data de entrada:                                                         ///\n");
-    scanf("%[0-9]/", dte);
+    scanf("%[0-9]/", alg->dte);
     getchar();
 
     printf("///          Data de saída:                                                           ///\n");
-    scanf("%[0-9]/", dts);
+    scanf("%[0-9]/", alg->dts);
     getchar();
 
     printf("///          Tipo do veículo:                                                         ///\n");
-    scanf("%[A-ZÁÉÍÓÚÂÊÔÇÀÃÕ a-záéíóúâêôçàãõ]", tpvei);
+    scanf("%[A-ZÁÉÍÓÚÂÊÔÇÀÃÕ a-záéíóúâêôçàãõ]", alg->tpvei);
     getchar();
 
     printf("///          Modelo do veículo:                                                       ///\n");
-    scanf("%[A-ZÁÉÍÓÚÂÊÔÇÀÃÕ a-záéíóúâêôçàãõ]", mdvei);
+    scanf("%[A-ZÁÉÍÓÚÂÊÔÇÀÃÕ a-záéíóúâêôçàãõ]", alg->mdvei);
     getchar();
 
     printf("///          Placa do veículo:                                                        ///\n");
-    scanf("%[A-Za-z0-9]", placa);
+    scanf("%[A-Za-z0-9]", alg->placa);
     getchar();
 
     printf("///                                                                                   ///\n");
     printf("/////////////////////////////////////////////////////////////////////////////////////////\n");
     printf("///                                                                                   ///\n");
     printf("///       Valor total do aluguel: R$                                                  ///\n");
-    scanf("%[0-9],.", valor);
+    scanf("%[0-9],.", alg->valor);
     printf("///                                                                                   ///\n");
     printf("/////////////////////////////////////////////////////////////////////////////////////////\n");
     printf("\n");
     printf("\t\t\t>>> Tecle <ENTER> para continuar...\n");
     getchar();
 }
-
