46258137d2cb8e81057ac7b6afd08c72f6bc99e7
JoaoVitorGomesDantas <92762467+JoaoVitorGomesDantas@users.noreply.github.com>
2022-01-24 19:07:13 -0300

    Update Validar.h

--- Validar.h
+++ Validar.h
@@ -1,3 +1,12 @@
+///////////////////////////////////////////////////////////////////////////////
+///               Universidade Federal do Rio Grande do Norte               ///
+///                   Centro de Ensino Superior do Seridó                   ///
+///                 Departamento de Computação e Tecnologia                 ///
+///                    Disciplina DCT1106 -- Programação                    ///
+///              Projeto Sistema de Controle de Estacionamento              ///
+///   Inspirado em: @flgorgonio https://github.com/flgorgonio/linguasolta   ///
+///                  Developed by @joaovitorgd - Out, 2021                  ///
+///////////////////////////////////////////////////////////////////////////////
 
 #define True 1
 #define False 0
@@ -7,3 +16,7 @@ char validaLetra (char);
 int validaTelefone(char*);
 int validarCPF (char*);
 int validarNome(char*);
+
+int ehBissexto(int);
+int ehData(int, int, int);
+int validarData(char*);
