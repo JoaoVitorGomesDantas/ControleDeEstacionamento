c8623debcd10b5a7be306e380a5fd74c92d79d85
JoaoVitorGomesDantas <92762467+JoaoVitorGomesDantas@users.noreply.github.com>
2022-02-19 14:49:56 -0300

    Update Gestão.h

--- Gestão.h
+++ Gestão.h
@@ -9,13 +9,44 @@
 ///                  Developed by @joaovitorgd - Out, 2021                  ///
 ///////////////////////////////////////////////////////////////////////////////
 
+#define True 1
+#define False 0
+
+typedef struct gestao Gestao;
+
+struct gestao {
+  char id[10];
+  char diaf[10];
+  char mesf[10];
+  char anof[10];
+  char ftd[15];
+  char dpd[15];
+  char lcd[15];
+  int status;
+};
 
 void moduloGestao(void);
 char telaGestao(void);
+
 void moduloFluxoFinanceiro(void);
+void anexarFluxoDeCaixa(void);
+void telaErroArquivoFluxoFinanceiro(void);
+void pesquisarFluxoDeCaixa(void);
+void atualizarFluxoDeCaixa(void);
+void excluirFluxoDeCaixa(void);
+void listarFluxoDeCaixa(void);
+void listaFluxoDeCaixaPorDia(void);
+void listaFluxoDeCaixaPorMes(void);
+
 char telaFluxoFinanceiro(void);
-void telaAnexarReceita(void);
-void telaAnexarVagas(void);
-void telaRelatorioSemanal(void);
-void telaRelatorioMensal(void);
-char* telaControleVagas(void);
+Gestao* telaAnexarReceita(void);
+char* telaPesquisarFluxoDeCaixa(void);
+char* telaAtualizarFluxoDeCaixa(void);
+char* telaExcluirFluxoDeCaixa(void);
+void telaRelatorioDiario(Gestao*);
+void telaRelatorioMensal(Gestao*);
+
+void salvarFluxoDeCaixa(Gestao*);
+Gestao* buscarFluxoDeCaixa(char*);
+void exibirFluxoDeCaixa(Gestao*);
+void regravarFluxoDeCaixa(Gestao*);
