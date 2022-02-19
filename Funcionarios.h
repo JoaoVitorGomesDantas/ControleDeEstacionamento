c5b37b627e620e0cb3dd3a6d720a398d43c54000
JoaoVitorGomesDantas <92762467+JoaoVitorGomesDantas@users.noreply.github.com>
2022-02-18 23:37:07 -0300

    Update Funcionarios.h

--- Funcionarios.h
+++ Funcionarios.h
@@ -8,6 +8,9 @@
 ///                  Developed by @joaovitorgd - Out, 2021                  ///
 ///////////////////////////////////////////////////////////////////////////////
 
+#define True 1
+#define False 0
+
 typedef struct funcionario Funcionario;
 
 struct funcionario {
@@ -15,9 +18,11 @@ struct funcionario {
     char cpf[14];
     char rg[14];
     char nasc[11];
+    char city[61];
     char end[61];
     char email[61];
     char fone[12];
+    char trn[21];
     int status;
 };
 
@@ -25,6 +30,8 @@ void cadastrarFuncionario(void);
 void pesquisarFuncionario(void);
 void atualizarFuncionario(void);
 void excluirFuncionario(void);
+void listarFuncionario(void);
+void listaFuncionarioPorTurno(void);
 
 void moduloFuncionarios(void);
 char telaFuncionarios(void);
