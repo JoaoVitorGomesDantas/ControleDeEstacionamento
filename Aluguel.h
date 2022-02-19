b0d3efbe0f11ae134d9a4917b93f6fe30bc1b1b1
JoaoVitorGomesDantas <92762467+JoaoVitorGomesDantas@users.noreply.github.com>
2022-02-19 03:09:02 -0300

    Update Aluguel.h

--- Aluguel.h
+++ Aluguel.h
@@ -9,23 +9,42 @@
 ///                  Developed by @joaovitorgd - Out, 2021                  ///
 ///////////////////////////////////////////////////////////////////////////////
 
+#define True 1
+#define False 0
 
 typedef struct aluguel Aluguel;
 
 struct aluguel {
   char nome[61];
+  char tipo[10];
   char cpf[14];
+  char rg[14];
   char dte[11];
   char dts[11];
   char tpvei[40];
-  char mdvei[40];
   char placa[12];
   char valor[12];
+  int status;
 };
 
+void cadastrarAluguel(void);
+void pesquisarAluguel(void);
+void atualizarAluguel(void);
+void excluirAluguel(void);
+void listarAluguel(void);
+void listaAlugueisPorTipo(void);
 
 void moduloAluguel(void);
 char telaAluguel(void);
+void telaErroArquivoAluguel(void);
 void telaVagas(void);
-void telaAlugando(void);
+Aluguel* telaAlugando(void);
+char* telaPesquisarAluguel(void);
+char* telaAtualizarAluguel(void);
+char* telaExcluirAluguel(void);
 void telaValores(void);
+
+void salvarAluguel(Aluguel*);
+Aluguel* buscarAluguel(char*);
+void exibirAluguel(Aluguel*);
+void regravarAluguel(Aluguel*);
